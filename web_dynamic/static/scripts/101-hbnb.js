a15023ba9c2c02590530a539a78475b0bef889c5
Manuelpv17 <manuelpv17@outlook.com>
2020-09-28 20:52:34 -0500

    Add task 7, show hide reviews

--- web_dynamic/static/scripts/101-hbnb.js
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -2,7 +2,7 @@ const $ = window.$;
 $(document).ready(function () {
   let dictiAmenities = {};
   let dictiLocations = {};
-  $(":button").click(function () {
+  $(':button').click(function () {
     const idsAmenities = [];
     const idsLocations = [];
     for (const id in dictiAmenities) {
@@ -12,102 +12,147 @@ $(document).ready(function () {
       idsLocations.push(`"${idLoc}"`);
     }
 
-    $.ajax("http://0.0.0.0:5001/api/v1/places_search/", {
-      type: "POST",
-      contentType: "application/json",
+    $.ajax('http://0.0.0.0:5001/api/v1/places_search/', {
+      type: 'POST',
+      contentType: 'application/json',
       data: `{"amenities": [${idsAmenities}], "cities":[${idsLocations}], "states":[${idsLocations}]}`,
-      dataType: "json",
+      dataType: 'json'
     }).done(function (data) {
-      $(".places ").text("");
+      $('.places ').text('');
       arr(data);
     });
   });
 
-  $(".amenities").on("change", "input[type='checkbox']", function () {
-    dictiAmenities = myList(this, ".amenities", dictiAmenities);
+  $('.amenities').on('change', "input[type='checkbox']", function () {
+    dictiAmenities = myList(this, '.amenities', dictiAmenities);
   });
 
-  $(".locations").on("change", "input[type='checkbox']", function () {
-    dictiLocations = myList(this, ".locations", dictiLocations);
+  $('.locations').on('change', "input[type='checkbox']", function () {
+    dictiLocations = myList(this, '.locations', dictiLocations);
   });
 
-  $.ajax("http://0.0.0.0:5001/api/v1/status", {
-    type: "GET",
+  $.ajax('http://0.0.0.0:5001/api/v1/status', {
+    type: 'GET'
   }).done(function (data) {
-    if (data.status === "OK") {
-      $("DIV#api_status").addClass("available");
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
     } else {
-      $("DIV#api_status").removeClass("available");
+      $('DIV#api_status').removeClass('available');
     }
   });
-  $.ajax("http://0.0.0.0:5001/api/v1/places_search/", {
-    type: "POST",
-    contentType: "application/json",
-    data: "{}",
-    dataType: "json",
+  $.ajax('http://0.0.0.0:5001/api/v1/places_search/', {
+    type: 'POST',
+    contentType: 'application/json',
+    data: '{}',
+    dataType: 'json'
   }).done(function (newData) {
     arr(newData);
   });
 });
 
-function myList(elem, myClass, dicti) {
+function myList (elem, myClass, dicti) {
   if (elem.checked === true) {
-    console.log(elem);
-    dicti[$(elem).data("id")] = $(elem).data("name");
+    dicti[$(elem).data('id')] = $(elem).data('name');
   } else {
-    delete dicti[$(elem).data("id")];
+    delete dicti[$(elem).data('id')];
   }
   const lista = [];
   for (const item in dicti) {
     lista.push(dicti[item]);
   }
   if (lista.length > 0) {
-    $("div" + myClass)
-      .find("h4")
-      .text(lista.join(", "));
+    $('div' + myClass)
+      .find('h4')
+      .text(lista.join(', '));
   } else {
-    $("div" + myClass)
-      .find("h4")
-      .html("&nbsp;");
+    $('div' + myClass)
+      .find('h4')
+      .html('&nbsp;');
   }
   return dicti;
 }
 
-function arr(newData) {
+function arr (newData) {
   let letra2, letra3, letra;
   for (const place of newData) {
     if (place.number_bathrooms === 1) {
-      letra = " Bathroom";
+      letra = ' Bathroom';
     } else {
-      letra = " Bathrooms";
+      letra = ' Bathrooms';
     }
     if (place.max_guest === 1) {
-      letra2 = " Guest";
+      letra2 = ' Guest';
     } else {
-      letra2 = " Guests";
+      letra2 = ' Guests';
     }
     if (place.number_rooms === 1) {
-      letra3 = " Bedroom";
+      letra3 = ' Bedroom';
     } else {
-      letra3 = " Bedrooms";
+      letra3 = ' Bedrooms';
     }
-    $(".places ").append(
-      '<article><div class="title_box"><h2>' +
-        place.name +
-        '</h2><div class="price_by_night">$' +
-        place.price_by_night +
-        '</div></div><div class="information"><div class="max_guest">' +
-        place.max_guest +
-        letra2 +
-        '</div><div class="number_rooms">' +
-        place.number_rooms +
-        letra3 +
-        '</div><div class="number_bathrooms">' +
-        place.number_bathrooms +
-        letra +
-        '</div></div><div class="description">' +
-        place.description +
-        "</div></article>"
-    );
+    const text = `
+      <article id='${place.id}'>
+        <div class='title_box'>
+          <h2>${place.name}</h2>
+          <div class='price_by_night'>${place.price_by_night}</div>
+        </div>
+        <div class='information'>
+          <div class='max_guest'>
+            ${place.max_guest} ${letra2}
+          </div>
+          <div class='number_rooms'>
+            ${place.number_rooms} ${letra3}
+          </div>
+          <div class='number_bathrooms'>
+            ${place.number_bathrooms} ${letra}
+          </div>
+        </div>
+        <div class='description'> ${place.description}</div>
+        <br>
+        <b>Reviews</b>
+        <span onclick='stateId("${place.id}")' class='show' id='review'>
+          Show
+        </span>
+        <div>
+          <ul class='reviews'></ul>
+        </div>
+      </article>`;
+
+    $('.places').append(text);
+  }
+}
+
+function stateId (id) {
+  if ($(`#${id} #review`).hasClass('show')) {
+    $(`#${id} #review`).removeClass('show');
+    $(`#${id} #review`).addClass('hide');
+    $(`#${id} #review`).text('hide');
+
+    $.ajax(`http://0.0.0.0:5001/api/v1/places/${id}/reviews`, {
+      type: 'GET'
+    }).done(function (data) {
+      let text = '';
+      $.each(data, function (index, value) {
+        const date = new Date(value.created_at);
+        $.get(`http://0.0.0.0:5001/api/v1/users/${value.user_id}`, function (
+          data
+        ) {
+          text += `
+          <li>
+            <b>${data.first_name}</b>
+            <p>${value.text}</p>
+            <p>${date}</p>
+          </li>
+        `;
+          $(`#${id} ul.reviews`).html(text);
+        });
+      });
+    });
+  } else {
+    $(`#${id} #review`).removeClass('hide');
+    $(`#${id} #review`).addClass('show');
+    $(`#${id} #review`).text('show');
+
+    $(`#${id} ul.reviews`).html('');
   }
 }
